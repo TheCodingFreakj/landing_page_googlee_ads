f3f6b95ac64cdc4d7b66142dd3e46ffdfdfb7b1e
unknown <pallavidapriya75@gmail.com>
2022-12-10 23:09:30 +0530

    value changed

--- server.js
+++ server.js
@@ -114,14 +114,14 @@ app.post("/serviceDetails", async (req, res) => {
   };
 
   function encrypt(text) {
-    var cipher = crypto.createCipher("aes-256-cbc", "d6F3Efeq");
+    var cipher = crypto.createCipheriv("aes-256-cbc", "d6F3Efeq");
     var crypted = cipher.update(text, "utf8", "hex");
     crypted += cipher.final("hex");
     return crypted;
   }
 
   function decrypt(text) {
-    var decipher = crypto.createDecipher("aes-256-cbc", "d6F3Efeq");
+    var decipher = crypto.createCipheriv("aes-256-cbc", "d6F3Efeq");
     var dec = decipher.update(text, "hex", "utf8");
     dec += decipher.final("utf8");
     return dec;
